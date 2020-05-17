cdbb85808822083118be079f6f60752c14ee766f
CarlosPenaDePedro <65302766+CarlosPenaDePedro@users.noreply.github.com>
2020-05-17 21:11:10 +0200

    Update Viatge.py

--- scripts/Viatge.py
+++ scripts/Viatge.py
@@ -1,6 +1,6 @@
 class Viaje:
     def __init__(self):
-        self.num_viajeros = 0  """ Numero de vaijeros"""
-        self.desinos  """ Lista/diccsionario de destinos """
+        self.num_viajeros = 0  """ Numero de viajeros"""
+        self.destinos  """ Lista/diccionario de destinos """
         self.datos_viajeros = []
 
